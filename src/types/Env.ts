0b1b911a07c4a4cde9f81cc9e204623924033c55
PADO <62028267+47PADO47@users.noreply.github.com>
2023-03-17 15:45:32 +0100

    🏷 Update Env interface

--- src/types/Env.ts
+++ src/types/Env.ts
@@ -1,3 +1,7 @@
-export interface Env {
-    env: "PRODUCTION" | "DEVELOPMENT"
+import { StorageEnv } from "@schiacciata/cf-workers-storage";
+
+export interface Env extends StorageEnv {
+    env: "PRODUCTION" | "DEVELOPMENT";
+	MY_KV_STORAGE: KVNamespace;
+	CUSTOM_KV: KVNamespace;
 };
