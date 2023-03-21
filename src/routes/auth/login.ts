8cc2bfea19d90bd1ce148ab259b5a9d86a442c43
PADO <62028267+47PADO47@users.noreply.github.com>
2023-03-21 23:10:52 +0100

    🛂 Create auth/login route

---
+++ src/routes/auth/login.ts
@@ -0,0 +1,47 @@
+import { User } from "../../types/Config";
+import { RouteHandleOptions } from "../../types/Route";
+import AuthBaseRoute from "./auth.base";
+
+type LoginPost = {
+    username?: string;
+    password?: string;
+}
+
+class LoginRoute extends AuthBaseRoute {
+    constructor() {
+        super('/login');
+    }
+
+    async handle(handleDTO: RouteHandleOptions): Promise<Response> {
+        if (!handleDTO.authenticator) return this.error('Authenticator not setup', 500);
+        
+        let body: LoginPost;
+        try {
+            body = await handleDTO.request.json();
+            if (!body.username || !body.password) return this.error('Missing username or password');
+        } catch (error) {
+            return this.error('Invalid body');
+        };
+
+        let jwt: string | undefined;
+        
+        try {
+            jwt = await handleDTO.authenticator.login<User>({
+                payload: {
+                    username: body.username,
+                    password: body.password,
+                }
+            });
+        } catch (error) {
+            return this.error('Error logging in: ' + error, 400)
+        }
+        
+        if (!jwt) {
+            return new Response('Unauthorized', { status: 401 });
+        };
+
+        return Response.json({ token: jwt });
+    }
+}
+
+export const route = new LoginRoute();
