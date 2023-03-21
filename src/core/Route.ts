6144bcae370039d1f60c6c2ff9be9dca06b6d948
PADO <62028267+47PADO47@users.noreply.github.com>
2023-03-21 22:48:17 +0100

    ✨ Add Route#error and auth param

--- src/core/Route.ts
+++ src/core/Route.ts
@@ -1,20 +1,28 @@
-import { RouteOptions, HTTPMethod } from "../types/Route";
-import { InterceptOptions } from "../types/Router";
+import { RouteOptions, HTTPMethod, RouteHandleOptions } from "../types/Route";
 import Path from "./Path";
 
 abstract class Route {
     path: Path;
     method: HTTPMethod;
     errorCode?: number;
+    auth?: boolean;
     constructor(options: RouteOptions) {
         this.path = options.path;
         this.method = options.method || "GET";
         if (options.errorCode) this.errorCode = options.errorCode;
         if (options.handle) this.handle = options.handle;
+        if (options.auth) this.auth = options.auth;
     };
     
-    handle(handleDTO: InterceptOptions): Promise<Response> | Response {
+    handle(handleDTO: RouteHandleOptions): Promise<Response> | Response {
         throw new Error("Not implemented");
+    }
+
+    error(message: string, status: number = 400): Response {
+        return Response.json({
+            error: message,
+            code: status
+        }, { status })
     }
 }
 
