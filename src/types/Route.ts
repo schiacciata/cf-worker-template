048781b383231b6b340624e9217ffa255cd5897f
PADO <62028267+47PADO47@users.noreply.github.com>
2023-03-21 22:47:58 +0100

    🏷️ Add auth param to Route

--- src/types/Route.ts
+++ src/types/Route.ts
@@ -1,11 +1,17 @@
+import { BearerAuthenticator } from "@schiacciata/cf-workers-auth";
 import Path from "../core/Path";
 import { InterceptOptions } from "./Router";
 
 export type HTTPMethod = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH";
 
+export type RouteHandleOptions = InterceptOptions & {
+    authenticator?: BearerAuthenticator,
+}
+
 export type RouteOptions = {
     path: Path,
     method?: HTTPMethod,
     errorCode?: number,
+    auth?: boolean,
     handle?: (handleDTO: InterceptOptions) => Promise<Response> | Response,
 }
