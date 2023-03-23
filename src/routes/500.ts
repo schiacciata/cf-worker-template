f8d7e7b1d57e7037b1d8c03c8a2969f1c0c7f6a2
PADO <62028267+47PADO47@users.noreply.github.com>
2023-03-23 20:24:51 +0100

    ✨ Create 500.ts route

---
+++ src/routes/500.ts
@@ -0,0 +1,20 @@
+import Path from "../core/Path";
+import Route from "../core/Route";
+import { RouteHandleOptions } from "../types/Route";
+
+export class ServerErrorRoute extends Route {
+    constructor() {
+        super({
+            path: new Path(),
+            errorCode: 500,
+        });
+    };
+
+    handle(handleDTO: RouteHandleOptions): Response | Promise<Response> {
+        return new Response(`An error happened, please try again`, {
+            status: 500,
+        });
+    }
+}
+
+export const route = new ServerErrorRoute();
