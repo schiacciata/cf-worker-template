1a7c0686cf8f4f79fa3a09c3dd4e5858be6a4c0b
PADO <62028267+47PADO47@users.noreply.github.com>
2023-03-23 20:26:09 +0100

    🥅 Catch worker errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import config from "./config";
 import Router from "./core/Router";
 import RoutesHandler from "./handlers/routes";
+import { ServerErrorRoute } from "./routes/500";
 import { Env } from "./types/Env";
 
 const router = new Router({})
@@ -14,11 +15,22 @@ export default {
 	): Promise<Response> {
 		config.setUp(env);
 
-		return await router.intercept({
+		const interceptParams = {
 			request,
 			env,
 			context: ctx,
 			config: config.getConfig(),
-		});
+		};
+
+		try {
+			return await router.intercept(interceptParams);
+		} catch (error) {
+			const errorRoute: ServerErrorRoute | undefined = await router.getErrorRoute(500);
+			if (!errorRoute) return new Response('An error happened: ' + error, {
+				status: 500,
+			});
+
+			return await errorRoute.handle(interceptParams);
+		};
 	},
 };
