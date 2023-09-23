254e28cad59f04cc470d023091c2b86d148f7a38
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-23 17:36:21 +0200

    🏷 Add fetchUserFromDB and admin to Route

--- src/types/Route.ts
+++ src/types/Route.ts
@@ -1,14 +1,29 @@
 import Path from "../core/Path";
 import { InterceptOptions } from "./Router";
+import { User } from "./db";
 
 export type HTTPMethod = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH";
 
-export type RouteHandleOptions = InterceptOptions & {}
+export type RouteHandleOptions = InterceptOptions & {
+    user?: User;
+}
+
+export interface IRoute {
+    path: Path,
+    method: HTTPMethod,
+    errorCode?: number,
+    auth: boolean,
+    fetchUserFromDB: boolean,
+    adminOnly: boolean,
+    handle: (handleDTO: InterceptOptions) => Promise<Response> | Response,
+}
 
 export type RouteOptions = {
     path: Path,
     method?: HTTPMethod,
     errorCode?: number,
     auth?: boolean,
+    fetchUserFromDB?: boolean,
+    adminOnly?: boolean,
     handle?: (handleDTO: InterceptOptions) => Promise<Response> | Response,
 }
