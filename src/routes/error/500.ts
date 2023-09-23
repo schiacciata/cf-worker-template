2e1dac95115224ccb18c02e3af5e344276e27cbc
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-23 16:45:11 +0200

    🏷 Create type ServerErrorRouteHanleOptions

--- src/routes/error/500.ts
+++ src/routes/error/500.ts
@@ -2,6 +2,10 @@ import Path from "@/core/Path";
 import Route from "@/core/Route";
 import { RouteHandleOptions } from "@/types/Route";
 
+export type ServerErrorRouteHanleOptions = RouteHandleOptions & {
+    error?: string | unknown,
+}
+
 export class ServerErrorRoute extends Route {
     constructor() {
         super({
@@ -10,10 +14,11 @@ export class ServerErrorRoute extends Route {
         });
     };
 
-    handle(handleDTO: RouteHandleOptions): Response | Promise<Response> {
+    handle(handleDTO: ServerErrorRouteHanleOptions): Response | Promise<Response> {
+        const error = handleDTO.error as Error;
         handleDTO.logger.error('Your cloudflare worker has encountered an error:', handleDTO.error);
         
-        return new Response(`An error happened, please try again`, {
+        return new Response(handleDTO.config.debug ? error.message : `An error happened, please try again`, {
             status: 500,
         });
     }
