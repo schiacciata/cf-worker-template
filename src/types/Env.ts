6435639a424c3bef4a342e45f3147c76595b6c57
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-23 16:37:29 +0200

    🏷 Add DB to Env

--- src/types/Env.ts
+++ src/types/Env.ts
@@ -1,8 +1,7 @@
-import { StorageEnv } from "@schiacciata/cf-workers-storage";
-
-export interface Env extends StorageEnv {
+export interface Env extends Record<string, any> {
     ENV: "PRODUCTION" | "DEVELOPMENT";
 	MY_KV_STORAGE: KVNamespace;
 	CUSTOM_KV: KVNamespace;
 	JWTsecret: string;
+	DB: D1Database;
 };
