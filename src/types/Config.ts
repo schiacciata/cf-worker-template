ecc73ba1d9ded95bf1a67110436ccc8258187c36
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-23 16:46:17 +0200

    🏷️ Remove User type

--- src/types/Config.ts
+++ src/types/Config.ts
@@ -1,11 +1,5 @@
 export type TConfig = {
     JWTSecret: string;
     debug?: boolean,
-    users: User[];
     JWTExpirationInS: number;
 };
-
-export type User = {
-    username: string;
-    password: string;
-}
