4eb34baa2c3ef70c6ae5e23e60017f7eac71c8e3
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-23 17:39:08 +0200

    ♻ Use handleDTO.user

--- src/routes/users/me.get.ts
+++ src/routes/users/me.get.ts
@@ -1,31 +1,15 @@
 import { RouteHandleOptions } from "@/types/Route";
 import UsersBaseRoute from "./users.base";
-import { db } from "@/core/db";
-import UserModel from "@/models/user";
 
 class UsersMeRoute extends UsersBaseRoute {
     constructor() {
         super('GET', '/me');
+        this.fetchUserFromDB = true;
     };
 
     async handle(handleDTO: RouteHandleOptions): Promise<Response> {
-		const token = handleDTO.authenticator.getTokenFromRequest(handleDTO.request);
-        if (!token) return this.error('Token not provided', 400);
-
-        let payload;
-        try {
-            payload = handleDTO.authenticator.getPayload(token);
-        } catch (error) {
-            return this.error('Could not decode payload', 400)
-        }
-        if (!payload) return this.error('No token payload found', 400);
-
-        const user = await new UserModel(db(handleDTO.env))
-            .find(payload.id);
-
-        if (!user) return this.error("User not found", 404);
-        
-        return Response.json(user);
+        if (!handleDTO.user) return this.error("Not logged in", 401);
+        return Response.json(handleDTO.user);
     }
 }
 
