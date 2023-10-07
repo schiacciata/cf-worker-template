6460fa049aab611835b405c0b2db183ba7908185
PADO <62028267+47PADO47@users.noreply.github.com>
2023-10-07 14:15:25 +0200

    ♻️ Add Handler#getModules

--- src/core/Handler.ts
+++ src/core/Handler.ts
@@ -7,14 +7,20 @@ abstract class Handler {
         if (options.handle) this.handle = options.handle;
     }
 
-    handle (): any | Promise<any> {
+    handle(): any | Promise<any> {
         throw new Error("Method not implemented");
     }
 
-    isModule (module?: NodeModule) {
+    private isModule (module?: NodeModule): module is NodeModule {
         if (!module || !module.exports || !module.loaded) return false;
         return Object.prototype.toString.call(module.exports) === '[object Module]';
     }
+
+    getModules(): NodeModule[] {
+        const cache = Object.values(require.cache);
+        const modules = cache.filter((m): m is NodeModule => this.isModule(m));
+        return modules;
+    }    
 }
 
 export default Handler;
